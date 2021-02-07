de461a7140bd6e0c2bdf421b71a5dc78e1d7f034
Anurag Raj <anurag.raj5@gmail.com>
2021-02-07 22:49:24 +0530

    Adding Dao Classes

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CategoryItemDao.java
@@ -0,0 +1,30 @@
+package com.upgrad.FoodOrderingApp.service.dao;
+
+import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CategoryItemEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+//This Class is created to access DB with respect to categoryItem entity
+
+@Repository
+public class CategoryItemDao {
+
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    //To get List of CategoryItemEntity by CategoryEntity if no result then null is returned
+    public List<CategoryItemEntity> getItemsByCategory(CategoryEntity categoryEntity) {
+        try {
+            List<CategoryItemEntity> categoryItemEntities = entityManager.createNamedQuery("getItemsByCategory",CategoryItemEntity.class).setParameter("category",categoryEntity).getResultList();
+            return categoryItemEntities;
+        }catch (NoResultException nre){
+            return null;
+        }
+    }
+}
